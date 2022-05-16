289f28f557282e7e66aed1be0a79d2044e905af7
Hanjiseok1 <heemoon25@naver.com>
2022-05-16 21:55:37 +0900

    연산 및 입력 공부

---
+++ JavaStudy_jiseok/src/j04_입력/Homework.java
@@ -0,0 +1,16 @@
+package j04_입력;
+
+import java.util.Scanner;
+// import
+public class Homework {
+
+	public static void main(String[] args) {
+		/*
+		 * 4의 배수 이면서(&&) 100의 배수가 아니거나(||) 400의 배수인 것
+		 * 참이면 윤년
+		 * 거짓이면 윤년이 아님
+		 */
+
+	}
+
+}
